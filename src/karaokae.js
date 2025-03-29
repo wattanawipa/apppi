9748193be5309ca631b7a64260ad82e0383e5c4e
wattanawipa <minn7714@gmail.com>
2025-03-29 13:59:45 +0700

    commit
    commit

---
+++ src/karaokae.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+
+export default function Karaokae(){
+    const textPswd = React.useRef()
+    const textPswd2 = React.useRef()
+
+    const onBlurPassword =() => {
+        let pswd = textPswd.current.value
+        if(pswd !== '' && !pswd.match()){
+            textPswd.current.value =''
+            alert('กรุณากรอกจำนวนชั่วโมง')  
+        }
+    }
+    const onBlurPassword2 = () =>{
+        if(textPswd.current.value !== textPswd2.current.value){
+            textPswd2.current.value =''
+            alert('กรุณากรอกจำนวนชั่วโมง')
+        }
+    }
+    return(
+        <div className="mt-4 mx-auto p-3 roundeb" 
+        style={{ width:'400px',background:'#cee'}}>
+            <form>
+                <div className="form-group mb-2">
+                    <label htmlFor="login">ชื่อผู้ใช้(Login)</label>
+                    <input type="text" id="login" name="login" maxLength="20" 
+                    className="form-control form-control-sm"/>
+                </div>
+                <div className="form-group mb-2">
+                    <label htmlFor="pswd">รหัสผ่าน</label>
+                    <input type="Password" id="pswd" name="pswd" maxLength="10" 
+                    className="form-control form-control-sm"
+                    ref={textPswd} onBlur={onBlurPassword}/>
+                </div>
+                <div className="form-group mb-2">
+                    <label htmlFor="pswd2">ใส่รหัสผ่านซ้ำอีกครั้ง</label>
+                    <input type="Password" id="pswd2"
+                    className="form-control form-control-sm"
+                    ref={textPswd2} onBlur={onBlurPassword2}/>
+                </div>
+                <div className="form-group mb-4">
+                    <label htmlFor="memo">บันทึกช่วยจำ</label>
+                    <textarea id="memo" name="memo" rows="2"
+                    maxLength="200"
+                    className="form-control form-control-sm">
+                    </textarea>
+                    </div>
+                    <div className="text-center">
+                        <button className="btn btn-sm px-4 btn-primary">
+                            OK
+                        </button>
+                </div>
+            </form>
+        </div> 
+)
+        
+}
